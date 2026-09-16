24d3b03851f5b692d71e86a7e9304ceec4c4d2f4
a01084782206-lgtm <a01084782206@gmail.com>
2026-09-16 13:44:25 +0900

    0916 chapter3

---
+++ Chapter2/06HTMLpageJAVA.js
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="KO">
+<head>
+    <meta charset="UTF-8">
+
+    <title>HTML5 basic</title>
+    <script>
+        alert('Hello JavaScript..!')
+    </script>
+
+    
+</head>
+<body>
+    <h1>Hello HTML5</h1>
+</body>
+</html>
